1adfad0ce6d99bd5a756d5222fe1d35066905f31
Dasteel <denisoid2013@yandex.ru>
2024-06-29 14:03:15 -0400

    Implement write function for storing secrets

--- src/secret_storage.c
+++ src/secret_storage.c
@@ -2,6 +2,8 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/slab.h>
+#include <linux/uaccess.h>
+
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Dasteel");
@@ -16,6 +18,29 @@ struct secret {
 
 static LIST_HEAD(secret_list);
 static int next_id = 1;
+
+tatic ssize_t write_secret(struct file *file, const char __user *buffer, size_t count, loff_t *ppos) {
+    struct secret *new_secret;
+    new_secret = kmalloc(sizeof(*new_secret), GFP_KERNEL);
+    if (!new_secret)
+        return -ENOMEM;
+
+    new_secret->data = kmalloc(count + 1, GFP_KERNEL);
+    if (!new_secret->data) {
+        kfree(new_secret);
+        return -ENOMEM;
+    }
+
+    if (copy_from_user(new_secret->data, buffer, count)) {
+        kfree(new_secret->data);
+        kfree(new_secret);
+        return -EFAULT;
+    }
+    new_secret->data[count] = '\0';
+    new_secret->id = next_id++;
+    list_add(&new_secret->list, &secret_list);
+    return count;
+}
 
 static int __init secret_init(void) {
     printk(KERN_INFO "Secret storage module loaded\n");
