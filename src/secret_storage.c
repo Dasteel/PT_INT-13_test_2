098e076dd31c3343f99736fbba0d6e52e22ca874
Dasteel <denisoid2013@yandex.ru>
2024-06-29 13:57:09 -0400

    Implement data structure for storing secrets

--- src/secret_storage.c
+++ src/secret_storage.c
@@ -1,11 +1,21 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/slab.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Dasteel");
 MODULE_DESCRIPTION("A simple Linux kernel module for storing secrets in procfs");
 MODULE_VERSION("v0.1.0-alpha");
+
+struct secret {
+    int id;
+    char *data;
+    struct list_head list;
+};
+
+static LIST_HEAD(secret_list);
+static int next_id = 1;
 
 static int __init secret_init(void) {
     printk(KERN_INFO "Secret storage module loaded\n");
